d403ddf0ecbbe22f45c3fde3ba750f1adceddad3
sergioodesigner <sergioodesigner@gmail.com>
2026-04-24 21:16:22 -0300

    fix: remove --os flag from gowa to prevent pairing rejection

--- gowa/manager.py
+++ gowa/manager.py
@@ -65,7 +65,6 @@ class GOWAManager:
         cmd.extend(["--webhook-events", "message,chat_presence,message.ack"])
         # Must be "available" to receive typing events from contacts
         cmd.extend(["--presence-on-connect", "available"])
-        cmd.extend(["--os", "Techify - WhatsBot"])
 
         logger.info("Starting GOWA: %s", " ".join(cmd))
         logger.info("GOWA working directory: %s", self.data_dir)
